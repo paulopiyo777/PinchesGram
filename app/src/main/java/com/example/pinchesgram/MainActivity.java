d4bae21a111ec4e11b5cbfacc2774b08c97a4cce
Paul Opiyo <59094545+paulopiyo777@users.noreply.github.com>
2020-10-30 15:56:50 -0500

    Implement more fragments

--- app/src/main/java/com/example/pinchesgram/MainActivity.java
+++ app/src/main/java/com/example/pinchesgram/MainActivity.java
@@ -24,6 +24,7 @@ import android.widget.Toast;
 
 import com.example.pinchesgram.fragments.ComposeFragment;
 import com.example.pinchesgram.fragments.PostFragment;
+import com.example.pinchesgram.fragments.ProfileFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.parse.FindCallback;
 import com.parse.ParseException;
@@ -56,17 +57,14 @@ public class MainActivity extends AppCompatActivity {
                 Fragment fragment;
                 switch (menuItem.getItemId()) {
                     case R.id.action_home:
-                        Toast.makeText(MainActivity.this, "Home!", Toast.LENGTH_SHORT).show();
                         fragment = new PostFragment();
                         break;
                     case R.id.action_compose:
-                        Toast.makeText(MainActivity.this, "Compose!", Toast.LENGTH_SHORT).show();
                         fragment = new ComposeFragment();
                         break;
                     case R.id.action_profile:
                     default:
-                        Toast.makeText(MainActivity.this, "Profile!", Toast.LENGTH_SHORT).show();
-                        fragment = new ComposeFragment();
+                        fragment = new ProfileFragment();
                         break;
                 }
                 fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
